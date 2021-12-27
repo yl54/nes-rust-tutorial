b97a1a0593ba8c76bf52725b67ab50faaa7d95e0
randyl <randyl@unity3d.com>
2021-12-26 19:56:04 -0800

    add codee

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,32 +1,54 @@
-// cpu struct
 pub struct CPU {
-	// --- Registers ---
-	// P - Program counter
+	// -------- Registers --------
+	// Small
+	// Fast access
 
-	// Stack Pointer
+	// PC - Program counter
+	// Holds address for next machine language instruction.
+	pub pc: u16,
+
+	// S - Stack Pointer
+	// Pointer to hold the address at the top of the stack.
+	pub s: u8,
 
 	// A - Accumulator
+	// Store result of arithmetic, logic, and memory access operations.
+	pub a: u8,
 
 	// X - Index register X
+	// It is the offset in specific memory addressing modes.
+	pub x: u8,
 
 	// Y - Index register Y
+	// Similar to X.
+	pub y: u8,
 
 	// P - Processor Status
+	// 8 bit register represents 7 status flags.
+	// Each one is toggled depending on operation.
+	pub p: u8,
 	
-	// Memory
+	// -------- Memory --------
+	// Slow
+	// Larger
+	pub mem: [u8; 0xFFFF],
 }
 
-// cpu impl
 impl CPU {
-	// new
+	// new creates a new CPU struct, default every register and memory to zero.
 	pub fn new() -> Self {
-		// init the fields to zero/default values
 		CPU {
-
+			pc: 0,
+			s: 0,
+			a: 0,
+			x: 0,
+			y: 0,
+			p: 0,
+			mem: [0; 0xFFFF],
 		}
 	}
 
-	// interpret 
+	// interpret interprets the incoming instructions.
 	// We need a mutable reference to the self, since the
 	// passed in value will change a register.
 	pub fn interpret(&mut self, program: Vec<u8>) {

f20e6770fae99fcaa457ebb3971c5dbc1bb69715
yl54 <randy.li54@outlook.com>
2021-12-30 14:53:35 -0800

    Chapter 3: Add Initial Interpret Code (#3)
    What
    
        Add initial interpret() code
        Add paths to handle BRK and LDA
        Add comments for other code to add in future MRs.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,10 @@
+// TODO: Implement this in another MR.
+// Addressing Modes
+	// List of Addressing Modes in Enum listing
+
+// A Table-like reference to hold information about ops codes.
+// TODO: Implement this in another MR.
+
 pub struct CPU {
 	// -------- Registers --------
 	// Small
@@ -23,15 +30,28 @@ pub struct CPU {
 	// Similar to X.
 	pub y: u8,
 
-	// P - Processor Status
-	// 8 bit register represents 7 status flags.
-	// Each one is toggled depending on operation.
+	/* P - Processor Status
+	 * 8 bit register represents 7 status flags.
+	 * Each one is toggled depending on operation.
+	 * 
+	 * 7 N Negative
+	 * 6 V Overflow
+	 * 5 - (Expansion)
+	 * 4 B Break Command
+	 * 3 D Decimal
+	 * 2 I Interrupt Disable
+	 * 1 Z Zero
+	 * 0 C Carry
+	 */
 	pub p: u8,
 	
 	// -------- Memory --------
 	// Slow
 	// Larger
 	pub mem: [u8; 0xFFFF],
+
+
+	// TODO: Consider having a handler object per ops code.
 }
 
 impl CPU {
@@ -48,10 +68,153 @@ impl CPU {
 		}
 	}
 
-	// interpret interprets the incoming instructions.
-	// We need a mutable reference to the self, since the
-	// passed in value will change a register.
+	// reset will restore all of the registers and memory to default states.
+	// TODO: Implement this in another MR.
+
+	// load will load the program's ROM into the designated memory space.
+	// TODO: Implement this in another MR.
+
+	// mem_read will read 2 bytes of whats at a memory position.
+	// TODO: Implement this in another MR.
+
+	// mem_write will write 2 bytes to a memory position.
+	// TODO: Implement this in another MR.
+
+	// mem_read_u16 will read 4 bytes of whats at a memory position.
+	// This function assumes data is stored in little endian.
+	// TODO: Implement this in another MR.
+
+	// mem_write_u16 will write  4 bytes to a memory position.
+	// This function assumes data is stored in little endian.
+	// TODO: Implement this in another MR.
+
+	// get_operand_address determines how an address should be read.
+	// It is determined based off of the Addressing mode.
+	// TODO: Implement this in another MR.
+
+	/*
+	 * interpret interprets the incoming instructions.
+	 * The basic loop is:
+	 * - fetch instruction from instruction memory
+	 * - decode the instruction
+	 * - execute the instruction
+	 * - go back to first step
+	 * 
+	 * We need a mutable reference to the self, since the
+	 * passed in value will change a register.
+	 */
 	pub fn interpret(&mut self, program: Vec<u8>) {
-		// fill it in
+		// Set the pc to 0, the start point.
+		self.pc = 0;
+
+		// Start the loop.
+		loop {
+			// Get the ops code from the pc.
+			// q: why convert from u16 to usize?
+			let opscode = program[self.pc as usize];
+
+			// Increment pc.
+			self.pc += 1;
+
+			// Execute based off of the ops code.
+			match opscode {
+				// Handle ops code LDA (0xA9).
+				// LDA is Load Accumulator.
+				0xA9 => {
+					// Get the param input from the next instruction.
+					let param = program[self.pc as usize];
+
+					// Increment pc.
+					self.pc += 1;
+
+					// Fill the A register with the param.
+					self.a = param;
+
+					// ---- Change the Processor Status Flags based off of the new A value -----
+					
+					// Check if the A register is 0.
+					if self.a == 0 {
+						// If 0, set the zero flag to 1.
+						self.p = self.p | 0b0000_0010;
+					} else {
+						// If not, set the zero flag to 0.
+						self.p = self.p & 0b1111_1101;
+					}
+
+					// Check if the A register is less than 0.
+					// It checks if the 7 bit of the a register value is set. If it's set, it's a negative number.
+					if self.a & 0b1000_0000 != 0 {
+						// If < 0, set the negative flag to 1.
+						self.p = self.p | 0b1000_0000;
+					} else {
+						// If >= 0, set the negative flag to 0.
+						self.p = self.p & 0b0111_1111;
+					}
+				}
+
+				// Handle ops code BRK (0x00).
+				// BRK is the break command. It causes an
+				// interrupt sequence. The program transfers control to the 
+				// interrupt vector.
+				0x00 => {
+					return;
+				}
+
+				// Handle ops code 2.
+				_ => {}
+			}
+		}
 	}
+}
+
+// --------- Tests ---------
+
+/* Typically, unit tests are listed below the actual code in the file.
+ * This is how rust works. It doesn't work if I put the test in a separate file.
+ */
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_LDA_happy_path() {
+        // Create a CPU.
+        let mut cpu = CPU::new();
+
+        // Interpret a short program.
+        // 1. Load a value into A register.
+        // 2. Break.
+        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+
+        // Check the A register has the expected value.
+        assert_eq!(cpu.a, 0x05);
+
+        // Check the Zero Flag is not set.
+        // Q: What is the difference between 0b00 and 0? 
+        assert!(cpu.p & 0b0000_0010 == 0b00);
+
+        // Check the Negative Flag is not set.
+        assert!(cpu.p & 0b1000_0000 == 0);
+    }
+
+    #[test]
+    fn test_LDA_zero() {
+        // Create a CPU.
+        let mut cpu = CPU::new();
+
+        // Interpret a short program.
+        // 1. Load zero into A register.
+        // 2. Break.
+        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+
+        // Check the A register has the expected value.
+        assert_eq!(cpu.a, 0x00);
+
+        // Check the Zero Flag is set.
+        assert!(cpu.p & 0b0000_0000 == 0);
+
+        // Check the Negative Flag is not set.
+        assert!(cpu.p & 0b0000_0000 == 0);
+    }
 }
